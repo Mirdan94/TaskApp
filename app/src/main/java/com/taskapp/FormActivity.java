8772381697dfba65ae98f18180c646dca0e8050d
Mirdan Akinov <akinovmirdan@gmail.com>
2019-10-02 12:10:28 +0600

    Icons Changed

---
+++ app/src/main/java/com/taskapp/FormActivity.java
@@ -0,0 +1,31 @@
+package com.taskapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class FormActivity extends AppCompatActivity {
+
+    EditText editTitle, editDesc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        editTitle = findViewById(R.id.editTitle);
+        editDesc = findViewById(R.id.editDesc);
+    }
+
+    public void onClick(View view) {
+        String title = editTitle.getText().toString().trim();
+        String desc = editDesc.getText().toString().trim();
+        Task task = new Task (title, desc);
+        Intent intent = new Intent();
+        intent.putExtra("task", task);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
